0bde1440958ade3fa7d6fe9fa2d0b34680a8d27b
H-Ramalan <talktoramalan@gmail.com>
2023-06-14 05:50:46 -0700

    todo list static page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,2 @@
-import _ from 'lodash';
-import './style.css';
-
-function component() {
-  const element = document.createElement('div');
-  const btn = document.createElement('button');
-
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
+import _ from "lodash";
+import "./style.css";
