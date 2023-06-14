2cf9a3666424c20f8b387e6b562dc258ef9ea8f5
H-Ramalan <talktoramalan@gmail.com>
2023-06-14 15:43:34 -0700

    fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,27 +1,26 @@
-import "./style.css";
+import './style.css';
 
-const todoList = document.querySelector(".todo-list");
+const todoList = document.querySelector('.todo-list');
 const todos = [
   {
     index: 1,
     completed: true,
-    task: "Wash the dish",
+    task: 'Wash the dish',
   },
   {
     index: 2,
     completed: true,
-    task: "Go to the gym",
+    task: 'Go to the gym',
   },
 ];
 
 function displayTasks() {
   todos.forEach((todos) => {
-    const li = document.createElement("li");
-    li.className = "list-item";
+    const li = document.createElement('li');
+    li.className = 'list-item';
 
-    li.innerHTML = `<input type="checkbox" class="check" /> ${todos.task} <span class="material-icons" id="close">
-close
-</span>`;
+    li.innerHTML = `<input type="checkbox" class="check" /> ${todos.task} <span class="material-icons" id="close">close</span>`;
+
     todoList.appendChild(li);
   });
 }
