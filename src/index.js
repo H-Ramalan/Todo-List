f6b3ff89064d86bd30e80421c00e51cecf46adcc
H-Ramalan <talktoramalan@gmail.com>
2023-06-15 07:22:32 -0700

    add/remove feature

--- src/index.js
+++ src/index.js
@@ -1,27 +1,49 @@
-import './style.css';
+import "./style.css";
+// import removeTask from "./modules/removetask";
+// import addTask from "./modules/addtasks";
 
-const todoList = document.querySelector('.todo-list');
-const todos = [
-  {
-    index: 1,
-    completed: true,
-    task: 'Wash the dish',
-  },
-  {
-    index: 2,
-    completed: true,
-    task: 'Go to the gym',
-  },
-];
+const add = document.querySelector(".btn");
+const todoList = document.querySelector(".todo-list");
+const deleteBtn = document.querySelector("#close");
+
+const todos = JSON.parse(localStorage.getItem("todos")) || [];
+
+function saveTasks() {
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+function addTask(e) {
+  const taskInput = document.querySelector(".task-input");
+  const newTask = {
+    id: todos.length + 1,
+    completed: false,
+    task: taskInput.value,
+  };
+  todos.push(newTask);
+  e.preventDefault();
+
+  saveTasks();
+}
+
+function removeTask(index) {
+  tasks.splice(index, 1);
+
+  for (let i = index; i < tasks.length; i++) {
+    tasks[i].index = i + 1;
+  }
+
+  saveTasks();
+}
+
+add.addEventListener("submit", addTask);
+deleteBtn.addEventListener("click", removeTask);
 
 function displayTasks() {
   todos.forEach((todos) => {
-    const li = document.createElement('li');
-    li.className = 'list-item';
-
+    const li = document.createElement("li");
+    li.className = "list-item";
     li.innerHTML = `<input type="checkbox" class="check" /> ${todos.task} <span class="material-icons" id="close">close</span>`;
-
     todoList.appendChild(li);
   });
+  saveTasks();
 }
 displayTasks();
